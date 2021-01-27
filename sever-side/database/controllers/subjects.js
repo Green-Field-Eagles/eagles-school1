a553f8902cc91fad7e82fa8143dd6df3c3b5d6cc
Sahar Al Issa <70060236+Saharalissa@users.noreply.github.com>
2021-01-28 01:14:48 +0200

    Update subjects.js

--- sever-side/database/controllers/subjects.js
+++ sever-side/database/controllers/subjects.js
@@ -3,12 +3,15 @@ var db = require('../database');
 const body= require('body-parser');
 var app = require('../../server/server');
 module.exports = {
+   // get all subjects
    getAllsubjects: (req, res)=> {
     models.getAllsubjects(function(err, results) {
         if (err) {console.log("error at subjects controller", err )}
         res.json(results);
     })
    },
+   
+   // create new subject
     createsubject: function(req, res) {
         var params = [req.body.subjectName];
         console.log(req.body.subjectName,"create");
@@ -17,6 +20,8 @@ module.exports = {
             res.sendStatus(200)
         });
     },
+   
+   // delete one subject based on id
     deleteOnesubject: function(req, res) {
         var params = [req.params.id];
         models.deleteOnesubject(params, function(err,results){
@@ -24,6 +29,8 @@ module.exports = {
             res.send('subject deleted')
         })
     },
+   
+   //get one subject by id
     getOnesubject: function(req, res) {
         var params = [req.params.id];
         models.getOnesubject(params, function(err, results) {
